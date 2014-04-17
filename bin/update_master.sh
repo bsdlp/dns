2285e6fd40851d58780258baa2b02e2f49269104
Jon Chen <bsd@voltaire.sh>
2014-04-16 22:33:20 -0400

    we dont need keys for git pull

--- bin/update_master.sh
+++ bin/update_master.sh
@@ -8,5 +8,5 @@ set -o errexit
 # https://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 readonly _DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ssh -A root@198.58.105.50 'cd /etc/nsd/ && git pull'
+ssh root@198.58.105.50 'cd /etc/nsd/ && git pull'
 /usr/local/sbin/nsd-control -c $_DIR/../local.conf reload
