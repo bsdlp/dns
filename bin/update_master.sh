a17b73a7e0730e7f532463ae8e1c17d5e302520b
Jon Chen <bsd@voltaire.sh>
2014-04-18 22:29:39 -0400

    copy over nsd.conf as well

--- bin/update_master.sh
+++ bin/update_master.sh
@@ -12,5 +12,6 @@ source $_DIR/../common.sh
 
 rsync -avz --progress $_DIR/../nsd/compiled/ $MASTER_USER@$MASTER_IP:$MASTER_PATH/zones/
 scp $_DIR/../nsd/zones.list $MASTER_USER@$MASTER_IP:$MASTER_PATH/zones.list
+scp $_DIR/../nsd/nsd.conf $MASTER_USER@$MASTER_IP:$MASTER_PATH/nsd.conf
 ssh $MASTER_USER@$MASTER_IP 'chown -R nsd:nsd /etc/nsd/'
 /usr/local/sbin/nsd-control -c $_DIR/../local.conf reload
