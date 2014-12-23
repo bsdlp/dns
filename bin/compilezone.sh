b414fa9950b3c1d92e95fc73b6aaed4fef0f7f0e
Jon Chen <bsd@voltaire.sh>
2014-12-23 12:19:28 -0800

    update compile-zone.sh

--- bin/compilezone.sh
+++ bin/compilezone.sh
@@ -3,4 +3,4 @@
 BIN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ZONE=$1
 
-named-compilezone -o ${DIR}/../nsd/compiled/${ZONE}.zone $ZONE ${DIR}/../nsd/zones/$ZONE
+named-compilezone -o ${BIN_DIR}/../nsd/compiled/${ZONE}.zone $ZONE ${BIN_DIR}/../nsd/zones/$ZONE
